759080dc368ecc955b936c3f66339efe570445ba
Alisher <kuanyshbekovalisher57@gmail.com>
2024-01-29 22:19:14 +0600

    add Constant

---
+++ Booking-Hotel/Supporting files/Constant/Constant.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Booking-Hotel
+//
+//  Created by Alisher on 13.01.2024.
+//
+
+import Foundation
+import UIKit
+
+enum Constant {
+    static let systemBounds = UIScreen.main.bounds
+    static let systemHeight = UIScreen.main.bounds.height
+    static let systemWidth = UIScreen.main.bounds.width
+}
